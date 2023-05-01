7a9fc338dd80d78c196874a1a9493b021101cd5d
JM/Karm <91932049+JMarlonKRM@users.noreply.github.com>
2023-05-01 20:35:08 -0300

    Add files via upload

---
+++ Zero-ao-cheio/exercicios-JPA/src/teste/NovoUsuario/NovoUsuario.java
@@ -0,0 +1,23 @@
+package teste.NovoUsuario;
+
+import modelo.base.Usuario;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class NovoUsuario {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-JPA");
+        EntityManager em = emf.createEntityManager();
+
+        Usuario novoUsuario = new Usuario("Djalma","djalma@lanche.com.br");
+
+        em.getTransaction().begin();
+        em.persist(novoUsuario);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+}
